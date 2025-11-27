5d044de151dcdaaeae7f71ec7f42270a31cc45d1
ITSTÖD\kjellh <kjell.hansen@gymnasium.ax>
2025-11-27 09:59:58 +0200

    Introduktion lektion 1

---
+++ hej.php
@@ -0,0 +1,8 @@
+<?php
+$namn='Kjell';
+$efternamn="Hansen";
+
+echo "Hej $namn";
+echo 'Hej $namn';
+echo "<p>Hej $efternamn</p>";
+echo '<p>Hej $efternamn';
---
+++ hej2.php
@@ -0,0 +1,8 @@
+<?php
+$namn='Kjell';
+$efternamn="Hansen";
+
+echo "Hej $namn<br>";
+echo 'Hej $namn<br>';
+echo "<p>Hej $efternamn</p>";
+echo '<p>Hej $efternamn</p>';
