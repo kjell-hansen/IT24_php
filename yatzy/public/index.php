d8e91b2322837da4c3f406490e7ffddbfc6af937
ITSTÖD\kjellh <kjell.hansen@gymnasium.ax>
2025-12-17 10:52:16 +0200

    Knapparna fungerar och tärningarna får nya värden

--- yatzy/public/index.php
+++ yatzy/public/index.php
@@ -1,3 +1,26 @@
+<?php
+declare(strict_types=1);
+
+// Initiera variabler
+$antalSlag = 0;
+$tarningar = [];
+$stanna = false;
+
+// Ta emot postat formulär
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $antalSlag = $_POST['antalSlag'];
+    if (isset($_POST['stanna'])) {
+        $stanna = true;
+    }
+} else {
+
+}
+$antalSlag++;
+for ($i = 1;$i <= 5;$i++) {
+    $tarningar[] = rand(1, 6);
+}
+
+?>
 <!doctype html>
 <html lang="sv">
     <head>
@@ -9,22 +32,42 @@
     <body>
         <h1>Yatzy</h1>
         <div id="tarningar">
-            <p id="tarning1">2</p>
-            <p id="tarning2">3</p>
-            <p id="tarning3">4</p>
-            <p id="tarning4">1</p>
-            <p id="tarning5">1</p>
+            <?php
+            foreach ($tarningar as $index => $value) {
+                echo "<p id='tarning$index'>$value</p>";
+            }
+            ?>
         </div>
-        <p>Behåll:</p>
-        <div id="checkboxes">
-            <p><input type="checkbox" name="check[1]" value="2"></p>
-            <p><input type="checkbox" name="check[2]" value="3"></p>
-            <p><input type="checkbox" name="check[3]" value="4"></p>
-            <p><input type="checkbox" name="check[4]" value="1"></p>
-            <p><input type="checkbox" name="check[5]" value="1"></p>
-        </div>
-        <p>Du har slagit 2 gånger</p>
-        <input type="submit" value="Slå igen">
-        <input type="submit" name="stanna" value="Stanna">
+        <?php
+        if (!$stanna) {
+            ?>
+            <p>Behåll:</p>
+            <form method="post">
+                <div id="checkboxes">
+                    <?php
+                    foreach ($tarningar as $index => $value) {
+                        echo "<p><input type='checkbox' name='check[$index]' value='$value'></p>";
+                    }
+                    ?>
+                </div>
+                <p>Du har slagit <?= $antalSlag; ?> gånger</p>
+                <input type="shidden" name="antalSlag" value="<?= $antalSlag; ?>">
+                <input type="submit" value="Slå igen">
+                <input type="submit" name="stanna" value="Stanna">
+            </form>
+            <?php
+        } else { ?>
+            <hr>
+            <p>Du fick:</p>
+            <ul>
+                <li>Ett par - värde 10</li>
+                <li>Chans - värde 23</li>
+            </ul>
+            <form>
+                <input type="submit" value="Börja om">
+            </form>
+        <?php
+        }
+        ?>
     </body>
 </html>
